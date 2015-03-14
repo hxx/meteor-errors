948f3a3d9e5bfffbdba7ff2b1e5291f49f0175d6
Star Huang <star@beansmile.com>
2015-03-14 14:54:33 +0800

    Update Git URL

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A pattern to display application errors to the user',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/hxx/meteor-errors',
+  git: 'https://github.com/hxx/meteor-errors.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: null
